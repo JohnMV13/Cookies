62ed8539101436eb211ff28a1764f80aec52cbfc
JohnMV13 <thejohnmiesterV@gmail.com>
2018-06-11 16:26:58 -0500

    home page looks better and started adding functions

--- app.js
+++ app.js
@@ -1,11 +1,63 @@
 function FirstandPike(max) {
-  return Math.floor(Math.random() * Math.floor());
+  return Math.floor(Math.random() * Math.floor(65));
 }
 
-console.log(sales(3));
+console.log(FirstandPike(3));
 // expected output: 0, 1 or 2
 
-console.log(sales(1));
+console.log(FirstadnPike(1));
+// expected output: 0
+
+console.log(Math.random());
+// expectedoutput: a number between 0 and 1
+
+function SeaTacAirport(max) {
+  return Math.floor(Math.random() * Math.floor(24));
+}
+
+console.log(SeaTacAirport(3));
+// expected output: 0, 1 or 2
+
+console.log(SeaTacAirport(1));
+// expected output: 0
+
+console.log(Math.random());
+// expectedoutput: a number between 0 and 1
+
+function SeattleCenter(max) {
+  return Math.floor(Math.random() * Math.floor(38));
+}
+
+console.log(SeattleCenter(3));
+// expected output: 0, 1 or 2
+
+console.log(SeattleCenter(1));
+// expected output: 0
+
+console.log(Math.random());
+// expectedoutput: a number between 0 and 1
+
+function CapitolHill(max) {
+  return Math.floor(Math.random() * Math.floor(38));
+}
+
+console.log(CapitolHill(3));
+// expected output: 0, 1 or 2
+
+console.log(CapitolHill(1));
+// expected output: 0
+
+console.log(Math.random());
+// expectedoutput: a number between 0 and 1
+
+function Alki(max) {
+  return Math.floor(Math.random() * Math.floor(16));
+}
+
+console.log(Alki(3));
+// expected output: 0, 1 or 2
+
+console.log(Alki(1));
 // expected output: 0
 
 console.log(Math.random());
