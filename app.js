28f38757266f6b4dcddd362c86723645a83eb481
JohnMV13 <thejohnmiesterV@gmail.com>
2018-06-11 15:21:00 -0500

    more work on pages

---
+++ app.js
@@ -0,0 +1,12 @@
+function FirstandPike(max) {
+  return Math.floor(Math.random() * Math.floor());
+}
+
+console.log(sales(3));
+// expected output: 0, 1 or 2
+
+console.log(sales(1));
+// expected output: 0
+
+console.log(Math.random());
+// expectedoutput: a number between 0 and 1
